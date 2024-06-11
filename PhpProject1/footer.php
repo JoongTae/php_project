66bf68520e1b330635e99adaa5d40126622018ba
JoongTae <xownd200225@gmail.com>
2024-06-11 15:44:02 +0900

    commit1

---
+++ PhpProject1/footer.php
@@ -0,0 +1,9 @@
+        <div id="footer_content">
+            <p id="footer_logo">음악 공연 홍보 및 예약 웹사이트<span></span></p>
+
+            <ul id="author">
+                <li>예약 관련 문의 메일</li>
+                <li>- 메일 주소 : kbw123@naver.com</li>
+            </ul>
+        </div>
+
---
+++ PhpProject1/member_insert.php
@@ -0,0 +1,33 @@
+<?php
+$id = $_POST["id"];
+$pass = $_POST["pass"];
+$name = $_POST["name"];
+$email1 = $_POST["email1"];
+$email2 = $_POST["email2"];
+$gender = $_POST["gender"];
+$hobbies = $_POST["hobby"];
+$age = $_POST["age"];
+$phone = $_POST["phone"];
+$address = $_POST["address"];
+$self = $_POST["self"];
+$interest = $_POST["interest"];
+
+$email = $email1."@".$email2;
+$regist_day = date("Y-m-d (H:i)"); // 현재의 '년-월-일-시-분'을 저장
+
+$hobby_str = implode(", ", $hobbies);
+
+$con = mysqli_connect("localhost", "user1", "12345", "sample");
+
+$sql = "insert into members(id, pass, name, email, regist_day, level, point, gender, hobby, age, phone, address, self) ";
+$sql .= "values('$id', '$pass', '$name', '$email', '$regist_day', 9, 0, '$gender', '$hobby_str', '$age', '$phone', '$address', '$self')";
+
+mysqli_query($con, $sql); // $sql 에 저장된 명령 실행
+mysqli_close($con);
+
+echo "
+<script>
+location.href = 'index.php';
+</script>
+";
+?>
